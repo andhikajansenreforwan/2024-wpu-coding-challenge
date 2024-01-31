7ffc3207692d552bd51945e4d42ab09b88364d7e
andhikajansenreforwan <jansendhika21@gmail.com>
2024-01-31 22:47:55 +0700

    day 7

---
+++ 7-CountByX.js
@@ -0,0 +1,12 @@
+// WPU Coding Challenge
+// 7/366
+// https://www.codewars.com/kata/5513795bd3fafb56c200049e
+
+function countBy(x, n) {
+    let z = [];
+    for(let i = 1; i <= n; i++)
+    z.push(i * x)
+    return z;
+  }
+
+  console.log(countBy(2,5))
